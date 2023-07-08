fd37571b5f44fac29b5cbd078830e0b558662e7f
avigangurde <avigangurde27@gmail.com>
2023-07-08 19:44:53 +0530

    my testcommit

---
+++ src/Birds.java
@@ -0,0 +1,21 @@
+public class Birds
+{
+    public String name;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    private String color;
+    public void display()
+    {
+        System.out.println("name is "+name+" and color is "+color);
+
+
+    }
+
+}
